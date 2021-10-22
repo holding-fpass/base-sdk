4d68ede9859c933b7f3bbc7e23723ba6b19d2e28
Guilheme M Pacheco <guilherme@fpass.com.br>
2021-10-22 10:51:38 -0300

    v1.3.7

--- src/schema/event/base.ts
+++ src/schema/event/base.ts
@@ -1,12 +1,8 @@
 import { ResourceBase, ResourceType } from "../resource";
 import { EventType } from "./type";
 import { v4 as uuidv4 } from "uuid";
-import { ApiProperty } from "@nestjs/swagger";
-
 export class BaseEvent extends ResourceBase {
-  @ApiProperty()
   public eventId: string = uuidv4();
-  @ApiProperty()
   public eventDate: string = new Date().toISOString();
 
   constructor(
--- src/schema/resource/base.ts
+++ src/schema/resource/base.ts
@@ -1,21 +1,9 @@
-import { ApiProperty } from "@nestjs/swagger";
 import { ResourceType } from "./type";
 
 export abstract class ResourceBase {
-  @ApiProperty()
-  public resourceId: string;
-  @ApiProperty()
-  public resourceType: ResourceType | string;
-  @ApiProperty()
-  public data?: any;
-
   constructor(
-    resourceId: string,
-    resourceType: ResourceType | string,
-    data?: any
-  ) {
-    this.resourceId = resourceId;
-    this.resourceType = resourceType;
-    this.data = data;
-  }
+    public resourceId: string,
+    public resourceType: ResourceType | string,
+    public data?: any
+  ) {}
 }
