a0ccfcedd5e6e349ee6fae5bdb039c07f0c4950b
Guilheme M Pacheco <guilherme@fpass.com.br>
2021-10-28 11:11:21 -0300

    v1.3.12

--- src/schema/event/type.ts
+++ src/schema/event/type.ts
@@ -1,7 +1,9 @@
 export enum EventType {
-  GenericEventCreated = 'generic.created',
-  DataEventCreated = 'data-event.created',
-  EmailCreated = 'email.created',
-  MFACreated = 'mfa.created',
-  MFAVerified = 'mfa.verified', 
+  GenericEventCreated = "generic.created",
+  DataEventCreated = "data-event.created",
+  EmailCreated = "email.created",
+  MFACreated = "mfa.created",
+  MFAVerified = "mfa.verified",
+  ClaimCreated = "claim.created",
+  ClaimAuthorized = "claim.authorized",
 }
