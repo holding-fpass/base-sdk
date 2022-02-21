dfe4d73eeaafcf16678305acb687aaed816cd0d5
Guilheme M Pacheco <guilherme@fpass.com.br>
2022-02-21 11:39:28 -0300

    feat: user whitelabel, scopes

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -1,10 +1,14 @@
 import { Metadata } from "./metadata";
 import { ProviderExtra } from "./provider";
+import { Scope } from "./scope";
+import { Whitelabel } from "./whitelabel";
 
 export interface User {
   name: string;
   email: string;
   taxId: string;
+  whitelabel: Whitelabel;
+  scopes: Scope[];
   image128x128?: string;
   providerExtra?: ProviderExtra[];
 }
