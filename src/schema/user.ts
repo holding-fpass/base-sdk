079be3d27859f5dc1d8905fb09ae611b2ef81ed4
Guilheme M Pacheco <guilherme@fpass.com.br>
2022-02-21 10:50:27 -0300

    feat: user fields

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -1,8 +1,11 @@
+import { Metadata } from "./metadata";
 import { ProviderExtra } from "./provider";
 
 export interface User {
   name: string;
   email: string;
+  taxId: string;
+  image128x128: string;
   providerExtra: ProviderExtra[];
 }
 
