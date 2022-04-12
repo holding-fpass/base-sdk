a89bdc747025983311d0e2c34e0b84018a1411d2
Guilheme M Pacheco <guilherme@fpass.com.br>
2022-04-12 15:04:09 -0300

    feat: InstanceThemeSettings, InstanceUrlSettings, InstanceParametersSettings

--- src/schema/instance.ts
+++ src/schema/instance.ts
@@ -32,6 +32,7 @@ export class Instance extends Resource<InstanceStatus> {
   urls!: Metadata[];
   theme!: Metadata[];
   features!: Metadata[];
+  parameters!: Metadata[];
   // Provider
   providerExtra?: ProviderExtra[];
 }
@@ -42,4 +43,77 @@ export enum InstanceFeatureFlags {
   CERTIFICATE = "certificate",
   LEARNING_ANALYTICS = "learning.analytics",
   COMMUNITY = "community",
+  WIZARD = "wizard",
+}
+
+export enum InstanceThemeSettings {
+  LOGO = "logo",
+  LOGOMARK = "logomark",
+  FAVICON = "favicon",
+  LOGODARK = "logoDark",
+  MOBILELOGO = "mobileLogo",
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+  TERTIARY = "tertiary",
+  GRADIENT = "gradient",
+  ERROR = "error",
+  WARNING = "warning",
+  INFO = "info",
+  SUCCESS = "success",
+  WHITE = "white",
+  GREY = "grey",
+  GRAY = "gray",
+  LIGHTGRAY = "lightGray",
+  LIGHTGRAY2 = "lightGray2",
+  DARKGRAY = "darkGray",
+  BACKGROUND = "background",
+  LIGHTBACKGROUND = "lightBackground",
+  DARKBACKGROUND = "darkBackground",
+  THIRD = "third",
+  SPOTLIGHT = "spotlight",
+  DARK = "dark",
+  DARK2 = "dark2",
+  DARK3 = "dark3",
+  DARKGREY = "darkGrey",
+  LIGHTGREY = "lightGrey",
+  LIGHTGREY2 = "lightGrey2",
+  BLACK = "black",
+  WHITE2 = "white2",
+  YELLOW = "yellow",
+  DARKERBLUR = "darkerBlur",
+  BLUR = "blur",
+  CARDCAROUSEL = "cardCarousel",
+  CARDCAROUSELBEFORE = "cardCarouselBefore",
+  GRADIENTHEADER = "gradientHeader",
+  OVERLAYDARKERBLUR = "overlayDarkerBlur",
+  OVERLAYLIGHTBLUR = "overlayLightBlur",
+  GREENCHECK = "greenCheck",
+  REDCHECK = "redCheck",
+  SHADOWOVERLAYTOP = "shadowOverlayTop",
+  SHADOWOVERLAYBOTTOM = "shadowOverlayBottom",
+  SHADOWOVERLAYMOBILEBOTTOM = "shadowOverlayMobileBottom",
+  OVERLAYDARKERMIDDLEBLUR = "overlayDarkerMiddleBlur",
+  OVERLAYGRADIENTPLAYER = "overlayGradientPlayer",
+}
+
+export enum InstanceUrlSettings {
+  FLABEL_URL = "flabel.url",
+  FCERTITIFICATE_URL = "certitificate.url",
+  CONTENT_API = "content.api",
+  FSTAGE_URL = "stage.url",
+  PRIVACYPOLICY_URL = "privacypolicy.url",
+  TERMSOFUSE_URL = "termsofuse.url",
+  CONTACTUS_URL = "contactus.url",
+  INSTAGRAM_URL = "instagram.url",
+  FACEBOOK_URL = "facebook.url",
+  YOUTUBE_URL = "youtube.url",
+  LINKEDIN_URL = "linkedin.url",
+}
+
+export enum InstanceParametersSettings {
+  GOOGLE_GTM_ID = "google.gtm.id",
+  GOOGLE_GA_ID = "google.ga.id",
+  ONESIGNAL_APP_ID = "onesignal.app.id",
+  FPASS_PLAYER_PAGEVIEW_INTERVAL = "fpass.player.pageview.interval",
+  FPASS_COPYRIGHT = "fpass.copyright",
 }
