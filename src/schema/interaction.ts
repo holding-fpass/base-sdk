ca695f90bf326fa1cfa2fd7f0d0146b2c1c11f29
ebertmota <ebert.mota03@gmail.com>
2022-07-12 15:22:56 -0300

    feat: add interationStatus to Interation schema

--- src/schema/interaction.ts
+++ src/schema/interaction.ts
@@ -1,12 +1,22 @@
 import { User } from "@sentry/node";
 import { Resource, ResourceType } from "./resource";
 
+export enum InteractionStatus {
+  CREATED = 'created',
+  ACTIVE = 'active'
+}
+
 export enum InteractionType {
   CLICK = "click",
   OPEN = "open",
   VIEW = "view",
 }
-export class Interaction extends Resource {
+
+export const InteractionStatusTransitionMap = new Map<InteractionStatus, InteractionStatus[]>(
+  [InteractionStatus.CREATED, [InteractionStatus.ACTIVE]]
+);
+
+export class Interaction extends Resource<InteractionStatus> {
   productId!: string;
   productType!: ResourceType;
   type!: InteractionType;
