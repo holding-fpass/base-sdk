d845e9314d208f66b34eddc0904d90401b6fcce2
Edinei Rodrigues Filho <edineibk@gmail.com>
2023-01-12 19:47:55 -0300

    chore(main): Added "provider.subscription.reactivated" and "reactivated" status on subscription schema.

--- src/schema/subscription.ts
+++ src/schema/subscription.ts
@@ -12,14 +12,16 @@ import { User } from "./user";
 import { Whitelabel } from "./whitelabel";
 
 export enum SubscriptionStatus {
-  CREATED = "created",
-  PROVIDER_SUBSCRIPTION_CREATED = "provider.subscription.created",
   ACTIVE = "active",
-  PROVIDER_SUBSCRIPTION_CANCELED = "provider.subscription.canceled",
   CANCELED = "canceled",
-  PROVIDER_SUBSCRIPTION_SUSPENDED = "provider.subscription.suspended",
-  SUSPENDED = "suspended",
+  CREATED = "created",
   DELETED = "deleted",
+  REACTIVATED = 'reactivated',
+  SUSPENDED = "suspended",
+  PROVIDER_SUBSCRIPTION_CANCELED = "provider.subscription.canceled",
+  PROVIDER_SUBSCRIPTION_CREATED = "provider.subscription.created",
+  PROVIDER_SUBSCRIPTION_REACTIVATED = 'provider.subscription.reactivated',
+  PROVIDER_SUBSCRIPTION_SUSPENDED = "provider.subscription.suspended",
 }
 
 export const SubscriptionStatusTransitionMap = new Map<
