5ff707e6beac5e9b2c7d2c8ac0b32bf6a1ec4b46
Tayanne <tayh.snovaes@gmail.com>
2023-01-31 12:56:59 -0300

    fix(notification): Pick items from Story

--- src/schema/notification.ts
+++ src/schema/notification.ts
@@ -55,7 +55,7 @@ export class Notification
   message?: NotificationMessage;
   // Related
   form?: Pick<Form, "resourceId" | "name">;
-  story?: Pick<Story, "resourceId" | "name" | "trigger">;
+  story?: Pick<Story, "resourceId" | "name" | "trigger" | "items">;
   campaign?: Pick<Campaign, "resourceId" | "name">;
   // Schedule
   notBeforeAt?: string | Timestamp;
