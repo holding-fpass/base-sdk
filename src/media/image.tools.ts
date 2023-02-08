7fe11fec8be14dd870c568c6a6357990a98e3f22
Guilherme M Pacheco <guilherme@fpass.com.br>
2023-02-08 07:54:23 -0300

    feat: document image tools

--- src/media/image.tools.ts
+++ src/media/image.tools.ts
@@ -1,4 +1,10 @@
 export class ImageUtils {
+  /**
+   * Image Otimized url
+   * @param url Google Storage public Url
+   * @param size Ex.: 256x128
+   * @returns Content API media public url
+   */
   static imageOptimized(url: string, size: string) {
     if (!url) return url;
     url = url.replace(
@@ -8,6 +14,12 @@ export class ImageUtils {
     const sizes = size.split("x");
     return url.concat(`?width=${sizes[0]}&height=${sizes[1]}`);
   }
+  /**
+   * Image Placeholder
+   * @param size Ex.: 128x128
+   * @param text Ex: 'Jonh Silva' will generate 'JS'
+   * @returns url string
+   */
   static imagePlaceholder(size: string, text?: string) {
     return `https://via.placeholder.com/${size}/?text=${text}`;
   }
