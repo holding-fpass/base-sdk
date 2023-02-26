a7de55510cfe724b3cfc251c4be32e7f26fcfa0a
Guilherme M Pacheco <guilherme@fpass.com.br>
2023-02-26 12:21:40 -0300

    fix: circular import

--- src/schema/display-resource.map.ts
+++ src/schema/display-resource.map.ts
@@ -5,13 +5,13 @@ import { Channel } from "./channel";
 import { Contract } from "./contract";
 import { Coupon } from "./coupon";
 import { Device } from "./device";
-import { Form } from "./form";
+import { Form, FormResponse } from "./form";
 import { Instance } from "./instance";
 import { Mfa } from "./mfa";
 import { Notification } from "./notification";
 import { Plan } from "./plan";
 import { Playlist } from "./playlist";
-import { DisplayResource, ResourceType } from "./resource";
+import { DisplayResource, Resource, ResourceType } from "./resource";
 import { Signature } from "./signature";
 import { Stage } from "./stage";
 import { Story } from "./story";
@@ -20,6 +20,10 @@ import { Subtitle } from "./subtitle";
 import { User } from "./user";
 import { Video } from "./video";
 import { Interaction } from "./interaction";
+import { Certificate } from "./certificate";
+import { Measurement } from "./measurement";
+import { Reaction } from "./reaction";
+import { Thread } from "./thread";
 
 export interface IDisplayResourceFunction extends Function {
   (resource: any): DisplayResource<any, any>;
@@ -52,4 +56,27 @@ export const DisplayResourceFunctionMap = new Map<
   [ResourceType.SUBTITLE, Subtitle.asDisplayResource],
   [ResourceType.USER, User.asDisplayResource],
   [ResourceType.VIDEO, Video.prototype.asDisplayResource],
+]);
+
+export const ResourceTypePrototypeMap = new Map<ResourceType, Resource>([
+  [ResourceType.CAMPAIGN, Campaign.prototype],
+  [ResourceType.CERTIFICATE, Certificate.prototype],
+  [ResourceType.CHANNEL, Channel.prototype],
+  [ResourceType.CONTENT, Content.prototype],
+  [ResourceType.CONTRACT, Contract.prototype],
+  [ResourceType.COUPON, Coupon.prototype],
+  [ResourceType.COURSE, Course.prototype],
+  [ResourceType.FORM_RESPONSE, FormResponse.prototype],
+  [ResourceType.INSTANCE, Instance.prototype],
+  [ResourceType.MEASUREMENT, Measurement.prototype],
+  [ResourceType.NOTIFICATION, Notification.prototype],
+  [ResourceType.PLAN, Plan.prototype],
+  [ResourceType.PLAYLIST, Playlist.prototype],
+  [ResourceType.REACTION, Reaction.prototype],
+  [ResourceType.STAGE, Stage.prototype],
+  [ResourceType.STORY, Story.prototype],
+  [ResourceType.SUBSCRIPTION, Subscription.prototype],
+  [ResourceType.SUBTITLE, Subtitle.prototype],
+  [ResourceType.THREAD, Thread.prototype],
+  [ResourceType.USER, User.prototype],
 ]);
