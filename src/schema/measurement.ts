3bef14b1f72ee42bbf7aa977bbf1e4441d872c4a
Tayanne <tayh.snovaes@gmail.com>
2023-03-24 10:04:01 -0300

    feat: add user progress rate to measurement schema

--- src/schema/measurement.ts
+++ src/schema/measurement.ts
@@ -60,6 +60,7 @@ export enum MeasurementType {
   USERS_ACTIVITY_DETAIL = "users.activity.detail",
   DEVICES_TOTAL_COUNT = "devices.total.count",
   USERS_ACTIVE_COUNT = "users.active.count",
+  USER_PROGRESS_RATE = "user.progress.rate",
   // Search
   SEARCH_QUERIES = "search.queries",
 }
