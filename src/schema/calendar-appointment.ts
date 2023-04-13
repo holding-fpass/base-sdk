adcc106cee4ab47eb1ecd3738a428635b8b42f5f
Samuel L <dev.samuellc@outlook.com>
2023-04-13 10:46:13 -0300

    feat: added enum CalendarDownloadFormats

--- src/schema/calendar-appointment.ts
+++ src/schema/calendar-appointment.ts
@@ -14,6 +14,11 @@ export enum AppointmentType {
   LIVE = "live"
 }
 
+export enum CalendarDownloadFormats {
+  ICS = "calendar.ics",
+  ICAL = "calendar.ical"
+}
+
 export class CalendarAppointment
   extends Resource<ContentType>
   implements SearchableResource {
