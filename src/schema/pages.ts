7c806de9fc44fe585d5817e35b06809f82af138f
Samuel L <dev.samuellc@outlook.com>
2023-04-17 09:20:15 -0300

    feat: getting playlists from pages/playlists

--- src/schema/pages.ts
+++ src/schema/pages.ts
@@ -23,7 +23,7 @@ export class Page extends Resource implements SearchableResource {
   name!: string;
   url!: string;
   // Related
-  playlists!: Pick<Playlist, "resourceId" | "name">[];
+  playlists!: Pick<Playlist, "resourceId" | "name" | "userTags">[];
   userTags?: Partial<Tag>[];
   userTags_idx?: string[];
   // SearchableResource implementation
