213f5b88fb7db6af7186f45cd0566db36f1daee5
Samuel L <dev.samuellc@outlook.com>
2023-05-26 11:09:21 -0300

    feat: added standard undefined value to notification.deletedAt ( in order to improve filtering )

--- src/schema/notification.ts
+++ src/schema/notification.ts
@@ -62,7 +62,7 @@ export class Notification
   form?: Pick<Form, "resourceId" | "name">;
   story?: Pick<Story, "resourceId" | "name">;
   campaign?: Pick<Campaign, "resourceId" | "name">;
-  deletedAt?: string | Timestamp;
+  deletedAt: string | Timestamp | undefined = undefined;
   // Schedule
   notBeforeAt?: string | Timestamp;
   deliveredAt?: string | Timestamp;
