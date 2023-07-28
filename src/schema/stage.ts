fbc34082f7be5aad74151b6da8a292a363fb4e30
Vinicius Vieira <viniciusbezvieira@gmail.com>
2023-07-28 18:36:41 -0300

    Remove parent propertie in Stage

--- src/schema/stage.ts
+++ src/schema/stage.ts
@@ -69,7 +69,6 @@ export class StagePayment {
 export class Stage extends Resource<StageStatus> implements SearchableResource {
   // Data
   resourceType = ResourceType.STAGE;
-  parentType?: ResourceType;
   name?: string;
   slug?: string;
   description?: string;
@@ -96,7 +95,6 @@ export class Stage extends Resource<StageStatus> implements SearchableResource {
     return {
       resourceType: ResourceType.STAGE,
       resourceId: resource.resourceId,
-      parentType: resource.parentType,
       h1: resource.name,
       status: resource.status,
       isPublic: resource.isPublic,
