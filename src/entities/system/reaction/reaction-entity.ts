c67236b2494b7a83f2c2d12795fc91c8ea39c511
Samuel L <dev.samuellc@outlook.com>
2023-10-05 10:20:54 -0300

    feat: added reaction entity/mapper

---
+++ src/entities/system/reaction/reaction-entity.ts
@@ -0,0 +1,110 @@
+import { ResourceType, Whitelabel } from 'schema';
+import { v4 as uuid } from 'uuid';
+
+export namespace IReactionClient {
+  export interface IHTTPReaction {
+    resourceId: string;
+    resourceType: ResourceType.REACTION;
+    userId: string;
+    contentId?: string | null;
+    courseId?: string | null;
+    postId?: string | null;
+    commentId?: string | null;
+    whitelabel: Whitelabel;
+    emoji: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+
+  export interface IProps {
+    resourceId: string;
+    resourceType: ResourceType.REACTION;
+    userId: string;
+    contentId?: string | null;
+    courseId?: string | null;
+    postId?: string | null;
+    commentId?: string | null;
+    whitelabel: Whitelabel;
+    emoji: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  export interface IConstructor {
+    resourceId: IReactionClient.IProps['resourceId'];
+    resourceType: IReactionClient.IProps['resourceType'];
+    userId: IReactionClient.IProps['userId'];
+    contentId?: IReactionClient.IProps['contentId'];
+    courseId?: IReactionClient.IProps['courseId'];
+    postId?: IReactionClient.IProps['postId'];
+    commentId?: IReactionClient.IProps['commentId'];
+    whitelabel: IReactionClient.IProps['whitelabel'];
+    emoji: IReactionClient.IProps['emoji'];
+    createdAt: IReactionClient.IProps['createdAt'];
+    updatedAt: IReactionClient.IProps['updatedAt'];
+  }
+}
+
+export class ReactionClient {
+  private props: IReactionClient.IProps;
+
+  public constructor(props: IReactionClient.IProps) {
+    this.props = {
+      ...props,
+      resourceId: props.resourceId || uuid(),
+      resourceType: ResourceType.REACTION,
+      emoji: props.emoji,
+      userId: props.userId,
+      whitelabel: props.whitelabel,
+      contentId: props.contentId || null,
+      courseId: props.courseId || null,
+      postId: props.postId || null,
+      commentId: props.commentId || null,
+      createdAt: props.createdAt,
+      updatedAt: props.updatedAt,
+    };
+  }
+
+  public get resourceId(): IReactionClient.IProps['resourceId'] {
+    return this.props.resourceId;
+  }
+
+  public get resourceType(): IReactionClient.IProps['resourceType'] {
+    return this.props.resourceType;
+  }
+
+  public get emoji(): IReactionClient.IProps['emoji'] {
+    return this.props.emoji;
+  }
+
+  public get userId(): IReactionClient.IProps['userId'] {
+    return this.props.userId;
+  }
+
+  public get whitelabel(): IReactionClient.IProps['whitelabel'] {
+    return this.props.whitelabel;
+  }
+
+  public get contentId(): IReactionClient.IProps['contentId'] {
+    return this.props.contentId;
+  }
+
+  public get courseId(): IReactionClient.IProps['courseId'] {
+    return this.props.courseId;
+  }
+
+  public get postId(): IReactionClient.IProps['postId'] {
+    return this.props.postId;
+  }
+
+  public get commentId(): IReactionClient.IProps['commentId'] {
+    return this.props.commentId;
+  }
+  public get createdAt(): IReactionClient.IProps['createdAt'] {
+    return this.props.createdAt;
+  }
+
+  public get updatedAt(): IReactionClient.IProps['updatedAt'] {
+    return this.props.updatedAt;
+  }
+}
---
+++ src/entities/system/reaction/reaction-mapper.ts
@@ -0,0 +1,35 @@
+import { ReactionClient, IReactionClient } from "./reaction-entity";
+
+export class ReactionClientMapper {
+  public static toApplication(reaction: IReactionClient.IHTTPReaction): ReactionClient {
+    return new ReactionClient({
+      resourceId: reaction.resourceId,
+      resourceType: reaction.resourceType,
+      userId: reaction.userId,
+      emoji: reaction.emoji,
+      commentId: reaction.commentId,
+      contentId: reaction.contentId,
+      courseId: reaction.courseId,
+      postId: reaction.postId,
+      whitelabel: reaction.whitelabel,
+      createdAt: new Date(reaction.createdAt),
+      updatedAt: new Date(reaction.updatedAt),
+    });
+  }
+
+  public static toHTTP(reaction: ReactionClient): IReactionClient.IHTTPReaction {
+    return {
+      resourceId: reaction.resourceId,
+      resourceType: reaction.resourceType,
+      userId: reaction.userId,
+      emoji: reaction.emoji,
+      commentId: reaction.commentId,
+      contentId: reaction.contentId,
+      courseId: reaction.courseId,
+      postId: reaction.postId,
+      whitelabel: reaction.whitelabel,
+      createdAt: reaction.createdAt.toISOString(),
+      updatedAt: reaction.updatedAt.toISOString(),
+    }
+  }
+}
