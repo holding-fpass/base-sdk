5b9fe397f6e1e1f473e8696cc5f3e497b8c3459e
Vinicius <viniciusbezvieira@gmail.com>
2023-10-06 18:43:48 -0300

    Post and Community properties in content

--- src/entities/content/content/content-entity.ts
+++ src/entities/content/content/content-entity.ts
@@ -1,7 +1,7 @@
 import { ResourceType, Whitelabel } from "../../../schema";
 import {
   ComplementaryMaterialClient,
-  IComplementaryMaterialClient
+  IComplementaryMaterialClient,
 } from "../complementary-material";
 
 export namespace IContentClient {
@@ -27,6 +27,8 @@ export namespace IContentClient {
     mentors: string[];
     tags: string[];
     courseId: string;
+    communityId: string | null;
+    postId: string | null;
     complementaryMaterials: IComplementaryMaterialClient.IHTTPComplementaryMaterial[];
     whitelabel: Whitelabel;
     metadata: Metadata | null;
@@ -35,12 +37,13 @@ export namespace IContentClient {
   }
 
   export enum EContentType {
-    VIDEO = 'video',
-    MEET = 'meet',
-    LIVE = 'live',
-    LINK = 'link',
-    EXAM = 'exam',
-    SLIDESHOW = 'slideshow',
+    VIDEO = "video",
+    MEET = "meet",
+    LIVE = "live",
+    LINK = "link",
+    EXAM = "exam",
+    SLIDESHOW = "slideshow",
+    POST = "post",
   }
 
   export namespace IClass {
@@ -66,6 +69,8 @@ export namespace IContentClient {
       mentors: string[];
       tags: string[];
       courseId: string;
+      communityId: string | null;
+      postId: string | null;
       complementaryMaterials: ComplementaryMaterialClient[];
       whitelabel: Whitelabel;
       metadata: Metadata | null;
@@ -75,27 +80,29 @@ export namespace IContentClient {
 
     export namespace IMethods {
       export interface IConstructor {
-        resourceId: IContentClient.IClass.IProps['resourceId'];
-        resourceType: IContentClient.IClass.IProps['resourceType'];
-        name: IContentClient.IClass.IProps['name'];
-        type: IContentClient.IClass.IProps['type'];
-        slug: IContentClient.IClass.IProps['slug'];
-        description: IContentClient.IClass.IProps['description'];
-        image144x80: IContentClient.IClass.IProps['image144x80'];
-        image1440x720: IContentClient.IClass.IProps['image1440x720'];
-        dateStart: IContentClient.IClass.IProps['dateStart'];
-        dateEnd: IContentClient.IClass.IProps['dateEnd'];
-        duration: IContentClient.IClass.IProps['duration'];
-        forms: IContentClient.IClass.IProps['forms'];
-        free: IContentClient.IClass.IProps['free'];
-        mentors: IContentClient.IClass.IProps['mentors'];
-        tags: IContentClient.IClass.IProps['tags'];
-        courseId: IContentClient.IClass.IProps['courseId'];
-        complementaryMaterials: IContentClient.IClass.IProps['complementaryMaterials'];
-        whitelabel: IContentClient.IClass.IProps['whitelabel'];
-        metadata: IContentClient.IClass.IProps['metadata'];
-        createdAt: IContentClient.IClass.IProps['createdAt'];
-        updatedAt: IContentClient.IClass.IProps['updatedAt'];
+        resourceId: IContentClient.IClass.IProps["resourceId"];
+        resourceType: IContentClient.IClass.IProps["resourceType"];
+        name: IContentClient.IClass.IProps["name"];
+        type: IContentClient.IClass.IProps["type"];
+        slug: IContentClient.IClass.IProps["slug"];
+        description: IContentClient.IClass.IProps["description"];
+        image144x80: IContentClient.IClass.IProps["image144x80"];
+        image1440x720: IContentClient.IClass.IProps["image1440x720"];
+        dateStart: IContentClient.IClass.IProps["dateStart"];
+        dateEnd: IContentClient.IClass.IProps["dateEnd"];
+        duration: IContentClient.IClass.IProps["duration"];
+        forms: IContentClient.IClass.IProps["forms"];
+        free: IContentClient.IClass.IProps["free"];
+        mentors: IContentClient.IClass.IProps["mentors"];
+        tags: IContentClient.IClass.IProps["tags"];
+        courseId: IContentClient.IClass.IProps["courseId"];
+        communityId: IContentClient.IClass.IProps["communityId"];
+        postId: IContentClient.IClass.IProps["postId"];
+        complementaryMaterials: IContentClient.IClass.IProps["complementaryMaterials"];
+        whitelabel: IContentClient.IClass.IProps["whitelabel"];
+        metadata: IContentClient.IClass.IProps["metadata"];
+        createdAt: IContentClient.IClass.IProps["createdAt"];
+        updatedAt: IContentClient.IClass.IProps["updatedAt"];
       }
     }
   }
@@ -108,87 +115,95 @@ export class ContentClient {
     this.props = props;
   }
 
-  public get resourceId(): IContentClient.IClass.IProps['resourceId'] {
+  public get resourceId(): IContentClient.IClass.IProps["resourceId"] {
     return this.props.resourceId;
   }
 
-  public get resourceType(): IContentClient.IClass.IProps['resourceType'] {
+  public get resourceType(): IContentClient.IClass.IProps["resourceType"] {
     return this.props.resourceType;
   }
 
-  public get name(): IContentClient.IClass.IProps['name'] {
+  public get name(): IContentClient.IClass.IProps["name"] {
     return this.props.name;
   }
 
-  public get type(): IContentClient.IClass.IProps['type'] {
+  public get type(): IContentClient.IClass.IProps["type"] {
     return this.props.type;
   }
 
-  public get slug(): IContentClient.IClass.IProps['slug'] {
+  public get slug(): IContentClient.IClass.IProps["slug"] {
     return this.props.slug;
   }
 
-  public get description(): IContentClient.IClass.IProps['description'] {
+  public get description(): IContentClient.IClass.IProps["description"] {
     return this.props.description;
   }
 
-  public get image144x80(): IContentClient.IClass.IProps['image144x80'] {
+  public get image144x80(): IContentClient.IClass.IProps["image144x80"] {
     return this.props.image144x80;
   }
 
-  public get image1440x720(): IContentClient.IClass.IProps['image1440x720'] {
+  public get image1440x720(): IContentClient.IClass.IProps["image1440x720"] {
     return this.props.image1440x720;
   }
 
-  public get dateStart(): IContentClient.IClass.IProps['dateStart'] {
+  public get dateStart(): IContentClient.IClass.IProps["dateStart"] {
     return this.props.dateStart;
   }
 
-  public get dateEnd(): IContentClient.IClass.IProps['dateEnd'] {
+  public get dateEnd(): IContentClient.IClass.IProps["dateEnd"] {
     return this.props.dateEnd;
   }
 
-  public get duration(): IContentClient.IClass.IProps['duration'] {
+  public get duration(): IContentClient.IClass.IProps["duration"] {
     return this.props.duration;
   }
 
-  public get forms(): IContentClient.IClass.IProps['forms'] {
+  public get forms(): IContentClient.IClass.IProps["forms"] {
     return this.props.forms;
   }
 
-  public get free(): IContentClient.IClass.IProps['free'] {
+  public get free(): IContentClient.IClass.IProps["free"] {
     return this.props.free;
   }
 
-  public get mentors(): IContentClient.IClass.IProps['mentors'] {
+  public get mentors(): IContentClient.IClass.IProps["mentors"] {
     return this.props.mentors;
   }
 
-  public get tags(): IContentClient.IClass.IProps['tags'] {
+  public get tags(): IContentClient.IClass.IProps["tags"] {
     return this.props.tags;
   }
 
-  public get courseId(): IContentClient.IClass.IProps['courseId'] {
+  public get courseId(): IContentClient.IClass.IProps["courseId"] {
     return this.props.courseId;
   }
 
-  public get complementaryMaterials(): IContentClient.IClass.IProps['complementaryMaterials'] {
+  public get communityId(): IContentClient.IClass.IProps["communityId"] {
+    return this.props.communityId;
+  }
+
+  public get postId(): IContentClient.IClass.IProps["postId"] {
+    return this.props.postId;
+  }
+
+  public get complementaryMaterials(): IContentClient.IClass.IProps["complementaryMaterials"] {
     return this.props.complementaryMaterials;
   }
 
-  public get whitelabel(): IContentClient.IClass.IProps['whitelabel'] {
+  public get whitelabel(): IContentClient.IClass.IProps["whitelabel"] {
     return this.props.whitelabel;
   }
 
-  public get metadata(): IContentClient.IClass.IProps['metadata'] {
+  public get metadata(): IContentClient.IClass.IProps["metadata"] {
     return this.props.metadata;
   }
 
-  public get createdAt(): IContentClient.IClass.IProps['createdAt'] {
+  public get createdAt(): IContentClient.IClass.IProps["createdAt"] {
     return this.props.createdAt;
   }
 
-  public get updatedAt(): IContentClient.IClass.IProps['updatedAt'] {
+  public get updatedAt(): IContentClient.IClass.IProps["updatedAt"] {
     return this.props.updatedAt;
   }
 }
