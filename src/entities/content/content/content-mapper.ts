63a75d4d93ac9dd4da689182661c32f765d2dda8
Vinicius <viniciusbezvieira@gmail.com>
2023-10-06 18:44:53 -0300

    Post and Community properties in content

--- src/entities/content/content/content-mapper.ts
+++ src/entities/content/content/content-mapper.ts
@@ -2,7 +2,9 @@ import { ComplementaryMaterialClientMapper } from "../complementary-material";
 import { ContentClient, IContentClient } from "./content-entity";
 
 export class ContentClientMapper {
-  public static toApplication(HTTPContent: IContentClient.IHTTPContent): ContentClient {
+  public static toApplication(
+    HTTPContent: IContentClient.IHTTPContent
+  ): ContentClient {
     return new ContentClient({
       resourceId: HTTPContent.resourceId,
       resourceType: HTTPContent.resourceType,
@@ -12,15 +14,24 @@ export class ContentClientMapper {
       description: HTTPContent.description,
       image144x80: HTTPContent.image144x80,
       image1440x720: HTTPContent.image1440x720,
-      dateStart: HTTPContent.dateStart ? new Date(HTTPContent.dateStart) : HTTPContent.dateStart as null,
-      dateEnd: HTTPContent.dateEnd ? new Date(HTTPContent.dateEnd) : HTTPContent.dateEnd as null,
+      dateStart: HTTPContent.dateStart
+        ? new Date(HTTPContent.dateStart)
+        : (HTTPContent.dateStart as null),
+      dateEnd: HTTPContent.dateEnd
+        ? new Date(HTTPContent.dateEnd)
+        : (HTTPContent.dateEnd as null),
       duration: HTTPContent.duration,
       forms: HTTPContent.forms,
       free: HTTPContent.free,
       mentors: HTTPContent.mentors,
       tags: HTTPContent.tags,
       courseId: HTTPContent.courseId,
-      complementaryMaterials: HTTPContent.complementaryMaterials.map((complementaryMaterial) => ComplementaryMaterialClientMapper.toApplication(complementaryMaterial)),
+      communityId: HTTPContent.communityId,
+      postId: HTTPContent.postId,
+      complementaryMaterials: HTTPContent.complementaryMaterials.map(
+        (complementaryMaterial) =>
+          ComplementaryMaterialClientMapper.toApplication(complementaryMaterial)
+      ),
       whitelabel: HTTPContent.whitelabel,
       metadata: HTTPContent.metadata,
       createdAt: new Date(HTTPContent.createdAt),
@@ -38,19 +49,28 @@ export class ContentClientMapper {
       description: content.description,
       image144x80: content.image144x80,
       image1440x720: content.image1440x720,
-      dateStart: content.dateStart ? content.dateStart.toISOString() : content.dateStart,
-      dateEnd: content.dateEnd ? content.dateEnd.toISOString() : content.dateEnd,
+      dateStart: content.dateStart
+        ? content.dateStart.toISOString()
+        : content.dateStart,
+      dateEnd: content.dateEnd
+        ? content.dateEnd.toISOString()
+        : content.dateEnd,
       duration: content.duration,
       forms: content.forms,
       free: content.free,
       mentors: content.mentors,
       tags: content.tags,
       courseId: content.courseId,
-      complementaryMaterials: content.complementaryMaterials.map((complementaryMaterial) => ComplementaryMaterialClientMapper.toHTTP(complementaryMaterial)),
+      communityId: content.communityId,
+      postId: content.postId,
+      complementaryMaterials: content.complementaryMaterials.map(
+        (complementaryMaterial) =>
+          ComplementaryMaterialClientMapper.toHTTP(complementaryMaterial)
+      ),
       whitelabel: content.whitelabel,
       metadata: content.metadata,
       createdAt: content.createdAt.toISOString(),
       updatedAt: content.updatedAt.toISOString(),
-    }
+    };
   }
 }
