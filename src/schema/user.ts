aa341717c53b74947fd8a19b58730db4e679115d
devsamuelc <dev.samuellc@outlook.com>
2024-04-18 18:49:02 -0300

    feat: reverted user channel change on main branch (temporary fix)

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -100,7 +100,7 @@ export class User extends Resource<UserStatus> implements SearchableResource {
   courses?: Partial<Course>[];
   transactions?: Partial<Transaction>[];
   // Channel
-  channelIds?: string[];
+  channel?: Pick<Channel, 'resourceId' | 'slug' | 'name'>;
   // Playlist
   favoritePlaylist?: Partial<Playlist>;
   suggestPlaylist?: Partial<Playlist>;
